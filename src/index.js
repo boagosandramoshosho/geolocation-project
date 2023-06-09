d170cb77a8ec5f406413bad16abdf939a8434a20
boagosandramoshosho <bmoshosho@yahoo.com>
2023-06-09 15:43:15 +0200

    added js file to get temperature of current location

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ function showTemp(response) {
   }
   
   function showPosition(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
     let apiKey = "6e4f1a7a1140d6ca523a88618d523748";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   
     axios.get(apiUrl).then(showTemp);
   }
